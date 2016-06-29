0e744f853e8b8285389235f34075b760ba5fcd81
brunaMoreira <bruna.r.moreira@hotmail.com>
2016-06-28 23:43:01 -0300

    AlgoritmoTeste

---
+++ PROVA 18 05 2016/exercicio2Promocao/exercicio2Promocao/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2Promocao
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //mmmmmm
+			Console.WriteLine("Digite a quantidade de rotulos: ");
+			int A = 0, B = 0, C = 0, D = 0, E = 0, Rotulo, qtdEnvelop = 0;
+			int QtdRotulos = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine("Digite os rotulos: ");
+
+			for(int i =1; i<= QtdRotulos; i++)
+			{
+				Console.WriteLine();
+				Rotulo = Convert.ToInt16(Console.ReadLine());
+				if(Rotulo == 1)
+				{
+					A++;
+				}
+				if (Rotulo == 2)
+				{
+					B++;
+				}
+				if (Rotulo == 3)
+				{
+					C++;
+				}
+				if (Rotulo == 4)
+				{
+					D++;
+				}
+				if (Rotulo == 5)
+				{
+					E++;
+				}
+				
+			}
+
+			qtdEnvelop = A;
+
+			if (qtdEnvelop > B)
+			{
+				qtdEnvelop = B;
+			}
+
+			if(qtdEnvelop > C)
+			{
+				qtdEnvelop = C;
+            }
+
+			if (qtdEnvelop > D)
+			{
+				qtdEnvelop = D;
+			}
+
+			if (qtdEnvelop > E)
+			{
+				qtdEnvelop = E;
+			}
+
+			Console.WriteLine("Você pode enviar " +qtdEnvelop + " envelopes");
+			Console.ReadKey();
+		}
+	}
+}
