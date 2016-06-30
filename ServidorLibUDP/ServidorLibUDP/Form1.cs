d465217a44398ee56336495b33af519f441f1912
brunaMoreira <bruna.r.moreira@hotmail.com>
2016-06-29 21:09:18 -0300

    udpservidor

---
+++ ServidorLibUDP/ServidorLibUDP/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUDP;
+
+namespace ServidorLibUDP
+{
+	public partial class Form1 : Form
+	{
+		private UDPSocket socket;
+
+		public Form1()
+		{
+			InitializeComponent();
+			socket = new UDPSocket(MensagemRecebida, 103);
+		}
+
+		private void MensagemRecebida(byte[] buffer, int size, string ip, int port)
+		{
+			//provaanterior
+			int soma = 0;
+			double media;
+
+			//percorre o vetor para somar os campos, pega a soma dos campos e adquiri a media. 
+
+			for (int i = 0; i < 10; i++)
+			{
+				soma = soma + Convert.ToInt16(buffer[i]);
+			}
+
+			media = soma / 10;
+
+			//percorre novamente o vetor para identificar o que são menores que a média. 
+
+			for (int i= 0; i < 10; i++)
+			{
+				if (Convert.ToInt16(buffer[i]) < media)
+				{
+					buffer[i] = Convert.ToByte("0");
+				}
+			}
+
+
+
+			socket.Send(buffer, ip, 6200);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+
+
+
+
+
+
+		}
+	}
+}
